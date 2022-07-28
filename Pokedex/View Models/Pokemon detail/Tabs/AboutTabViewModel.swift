c1acad9d0576015edae9beae871ce5f92f4307c3
Tino Tusaumwe <tusatino@gmail.com>
2022-07-28 16:43:09 +1000

    Remove models and moved code to view models

--- Pokedex/View Models/Pokemon detail/Tabs/AboutTabViewModel.swift
+++ Pokedex/View Models/Pokemon detail/Tabs/AboutTabViewModel.swift
@@ -8,51 +8,97 @@
 import SwiftUI
 
 final class AboutTabViewModel: ObservableObject {
-    @Published var model: AboutTabModel
+    @Published private var pokemon: Pokemon
+    @Published private(set) var pokemonSpecies: PokemonSpecies?
+    @Published private(set) var eggGroups = [EggGroup]()
+    @Published private(set) var abilities = [Ability]()
     
     init(pokemon: Pokemon) {
-        model = AboutTabModel(pokemon: pokemon)
+        self.pokemon = pokemon
     }
     
-    @MainActor
+    /// Sets up the view model.
     func setUp() async {
-        await model.setUp()
+        pokemonSpecies = await PokemonSpecies.fromName(name: pokemon.name)
+        abilities = await getPokemonAbilities()
+        await getEggGroups()
     }
-    
+}
+
+// MARK: Computed properties
+extension AboutTabViewModel {
     var pokemonSeedType: String {
-        model.pokemonSeedType
+        guard let pokemonSpecies else { return "Error" }
+        return pokemonSpecies.seedType
     }
     
+    /// The localized names for the pokemons egg group.
     var eggGroupNames: String {
-        model.eggGroupNames
+        var names = [String]()
+        for eggGroup in eggGroups {
+            let availableLanguages = eggGroup.names.map { name in
+                name.language.name
+            }
+            let deviceLanguageCode = Bundle.preferredLocalizations(from: availableLanguages).first!
+            let name = eggGroup.names.first { name in
+                name.language.name == deviceLanguageCode
+            }
+            if let name {
+                names.append(name.name)
+            }
+        }
+        return ListFormatter.localizedString(byJoining: names)
     }
 
+    /// The number for the pokemon in the pokedex.
     var pokemonID: Int {
-        model.pokemonID
+        pokemon.id
     }
-
+    
+    /// The pokemon's height in meters.
     var pokemonHeight: Double {
-        model.pokemonHeight
+        Double(pokemon.height) / 10.0
     }
     
+    /// The pokemon's weight in kilograms.
     var pokemonWeight: Double {
-        model.pokemonWeight
+        Double(pokemon.weight) / 10.0
     }
     
+    /// The localized names for the pokemon's abilities.
     var pokemonAbilities: String {
-        model.pokemonAbilities
+        var abilityNames = [String]()
+        for ability in abilities {
+            let availableLanguageCodes = ability.names.map { name in
+                name.language.name
+            }
+            let deviceLanguageCode = Bundle.preferredLocalizations(from: availableLanguageCodes).first!
+            let matchingAbility = ability.names.first { abilityName in
+                abilityName.language.name == deviceLanguageCode
+            }
+            if let matchingAbility {
+                abilityNames.append(matchingAbility.name)
+            } else {
+                abilityNames.append(ability.name)
+            }
+        }
+        return ListFormatter.localizedString(byJoining: abilityNames)
     }
     
+    /// The percentage chance for the male pokemon.
     var pokemonMaleGenderPercentage: Double {
-        model.pokemonMaleGenderPercentage
+        if pokemonSpecies == nil { return 0.0 }
+        return 1 - pokemonFemaleGenderPercentage
     }
     
+    /// The percentage chance for the female pokemon.
     var pokemonFemaleGenderPercentage: Double {
-        model.pokemonfemaleGenderPercentage
+        guard let pokemonSpecies else { return 0.0 }
+        return Double(pokemonSpecies.genderRate) / 8.0
     }
     
     var pokemonDescription: String {
-        guard let species = model.pokemonSpecies else {
+        guard let species = pokemonSpecies else {
             return "Error"
         }
         let availableLanguages = species.flavorTextEntries.map { entry in
@@ -66,5 +112,31 @@ final class AboutTabViewModel: ObservableObject {
             return matchedEntry.flavorText
         }
         return "No description available."
+    }
+}
+
+// MARK: Private functions
+private extension AboutTabViewModel {
+    /// Gets the abilities of this pokemon.
+    func getPokemonAbilities() async -> [Ability] {
+        var abilities = [Ability]()
+        for pokemonAbility in pokemon.abilities {
+            guard let ability = await Ability.from(name: pokemonAbility.ability.name) else { continue }
+            abilities.append(ability)
+        }
+        return abilities
+    }
+    
+    /// Gets all the egg groups for this pokemon.
+    func getEggGroups() async {
+        if pokemonSpecies == nil {
+            return
+        }
+        for eggGroups in pokemonSpecies!.eggGroups {
+            guard let group = await EggGroup.from(name: eggGroups.name) else {
+                continue
+            }
+            self.eggGroups.append(group)
+        }
     }
 }
--- Pokedex/View Models/Pokemon detail/Tabs/StatsTabViewModel.swift
+++ Pokedex/View Models/Pokemon detail/Tabs/StatsTabViewModel.swift
@@ -14,66 +14,161 @@ struct ValuePerStat: Identifiable {
     let id = UUID().uuidString
 }
 
-
 final class StatsTabViewModel: ObservableObject {
-    @Published private var model: StatsTabModel
     @Published private(set) var valuePerStat = [ValuePerStat]()
-
+    @Published private var pokemon: Pokemon
+    @Published private(set) var hpStat: Stat?
+    @Published private(set) var attackStat: Stat?
+    @Published private(set) var specialAttackStat: Stat?
+    @Published private(set) var doubleDamageTo = [`Type`]()
+    @Published private(set) var doubleDamageFrom = [`Type`]()
+    @Published private(set) var types = [`Type`]()
+    
     init(pokemon: Pokemon) {
-        model = StatsTabModel(pokemon: pokemon)
+        self.pokemon = pokemon
     }
     
     @MainActor
     func setUp() async {
-        await model.setUp()
+        hpStat = await getHPStat()
+        attackStat = await getAttackStat()
+        specialAttackStat = await getSpecialAttack()
+        types = await getTypes()
+        doubleDamageTo = await doubleDamageTo()
+        doubleDamageFrom = await doubleDamageFrom()
+        
         valuePerStat = [
-            .init(name: "HP", value: model.hp, colour: "hp"),
-            .init(name: "Attack", value: model.attack, colour: "attack"),
-            .init(name: "Defense", value: model.defense, colour: "defense"),
-            .init(name: "Sp. Attack", value: model.specialAttack, colour: "specialAttack"),
-            .init(name: "Sp. Defense", value: model.specialDefense, colour: "specialDefense"),
-            .init(name: "Speed", value: model.speed, colour: "speed"),
+            .init(name: "HP", value: hp, colour: "hp"),
+            .init(name: "Attack", value: attack, colour: "attack"),
+            .init(name: "Defense", value: defense, colour: "defense"),
+            .init(name: "Sp. Attack", value: specialAttack, colour: "specialAttack"),
+            .init(name: "Sp. Defense", value: specialDefense, colour: "specialDefense"),
+            .init(name: "Speed", value: speed, colour: "speed"),
         ]
     }
 }
 
+// MARK: - Computed properties
 extension StatsTabViewModel {
-    var hp: Int {
-        model.hp
-    }
-    var attack: Int {
-        model.attack
+    var hpStatName: String {
+        guard let hpStat else { return "Error" }
+        let availableLanguageCodes = hpStat.names.map { name in
+            name.language.name
+        }
+        let deviceLanguageCode = Bundle.preferredLocalizations(from: availableLanguageCodes).first!
+        let matchingName = hpStat.names.first { name in
+            return name.language.name == deviceLanguageCode
+        }
+        guard let matchingName else { return "Error" }
+        return matchingName.name
     }
     
-    var hpStatName: String {
-        model.hpStatName
+    var hp: Int {
+        let hpStat = pokemon.stats.first { stat in
+            stat.stat.name == "hp"
+        }
+        return hpStat?.baseStat ?? 0
+    }
+    
+    var attack: Int {
+        let attackStat = pokemon.stats.first { stat in
+            stat.stat.name == "attack"
+        }
+        return attackStat?.baseStat ?? 0
     }
     
     var defense: Int {
-        model.defense
+        let defenseStat = pokemon.stats.first { stat in
+            stat.stat.name == "defense"
+        }
+        return defenseStat?.baseStat ?? 0
     }
     
     var specialAttack: Int {
-        model.specialAttack
+        let specialAttack = pokemon.stats.first { stat in
+            stat.stat.name == "special-attack"
+        }
+        return specialAttack?.baseStat ?? 0
     }
     
     var specialDefense: Int {
-        model.specialDefense
+        let specialDefense = pokemon.stats.first { stat in
+            stat.stat.name == "special-defense"
+        }
+        return specialDefense?.baseStat ?? 0
     }
     
     var speed: Int {
-        model.speed
+        let speed = pokemon.stats.first { stat in
+            stat.stat.name == "speed"
+        }
+        return speed?.baseStat ?? 0
     }
     
     var totalStats: Int {
-        model.totalStats
+        pokemon.totalStats
+    }
+}
+
+// MARK: Private functions
+private extension StatsTabViewModel {
+    /// Gets the types for this pokemon.
+    func getTypes() async -> [`Type`] {
+        var results = [`Type`]()
+        for type in pokemon.types {
+            guard let type = await `Type`.from(name: type.type.name) else {
+                continue
+            }
+            results.append(type)
+        }
+        return results
     }
     
-    var doubleDamageFrom: [`Type`] {
-        model.doubleDamageFrom.sorted()
+    func getAttackStat() async -> Stat? {
+        let attackStat = pokemon.stats.first { stat in
+            stat.stat.name == "attack"
+        }
+        guard let attackStat else { fatalError("Fatal error in \(#function)\nInvalid pokemon stat") }
+        return await Stat.from(name: attackStat.stat.name)
     }
     
-    var doubleDamageTo: [`Type`] {
-        model.doubleDamageTo.sorted()
+    func getHPStat() async -> Stat? {
+        let hpStat = pokemon.stats.first { stat in
+            stat.stat.name == "hp"
+        }
+        guard let hpStat else { fatalError("Fatal error in \(#function)\nInvalid pokemon stat") }
+        return await Stat.from(name: hpStat.stat.name)
+    }
+    
+    func getSpecialAttack() async -> Stat? {
+        let spAttackStat = pokemon.stats.first { stat in
+            stat.stat.name  == "special-attack"
+        }
+        guard let spAttackStat else { fatalError("Fatal error in \(#function)\nInvalid pokemon stat.") }
+        return await Stat.from(name: spAttackStat.stat.name)
+    }
+    
+    /// Gets the types that this pokemon's type is weak against.
+    func doubleDamageFrom() async -> [`Type`] {
+        var results = Set<`Type`>()
+        for type in types {
+            for type in type.damageRelations.doubleDamageFrom {
+                guard let type = await `Type`.from(name: type.name) else { continue }
+                results.insert(type)
+            }
+        }
+        return Array(results)
+    }
+    
+    /// Gets the types that this pokemon's type is strong against.
+    func doubleDamageTo() async -> [`Type`] {
+        var results = Set<`Type`>()
+        for type in types {
+            for type in type.damageRelations.doubleDamageTo {
+                guard let type = await `Type`.from(name: type.name) else { continue }
+                results.insert(type)
+            }
+        }
+        return Array(results)
     }
 }
